47df5b7d9755e4bc8b46937a4d6d54d058cc47c0
moriel569 <moriel569@gmail.com>
2020-10-23 02:46:20 +0300

    Webpack configured

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import reactDOM from "react-dom";
+
+import "./styles/style.css";
+
+const App = () => {
+  return <h1>Webpack started</h1>;
+};
+
+reactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,25 @@ const path = require("path");
 
 module.exports = {
   devServer: {
-    contentBase: path.resolve(__dirname, "./src"),
+    contentBase: path.resolve(__dirname, "public"),
     historyApiFallback: true,
   },
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: "./src/app.js",
   module: {
     rules: [
       {
         test: /\.js$/,
         use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   output: {
+    path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
 };
